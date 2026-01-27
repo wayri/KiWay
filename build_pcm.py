031b9531f369eaedd85daa7c17e2feb8fa9cc823
Wayri <wayri@local>
2026-01-28 03:59:57 +0530

    fix: force new build with version.txt to clear cache

--- build_pcm.py
+++ build_pcm.py
@@ -35,6 +35,8 @@ def create_plugin_zip(plugin_path, version, output_dir, metadata):
                 # Nest under identifier folder
                 rel_path = os.path.relpath(file_path, start=plugin_path)
                 arcname = os.path.join(metadata['identifier'], rel_path)
+                # FORCE FORWARD SLASHES for Zip Standard
+                arcname = arcname.replace(os.sep, '/')
                 zipf.write(file_path, arcname)
     
     return zip_path, zip_filename
